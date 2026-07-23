00340227ab6adec3248334aaafdcca539d38f525
Dare <8192966+daredevil28@users.noreply.github.com>
2026-07-23 04:29:39 +0200

    Added return to input and fixed up/down movement

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -14,6 +14,7 @@ var current_rock : RigidBody3D
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.use_accumulated_input = false
 	ray = get_node("Camera3D/RayCast3D")
 	collect_rock_label = get_tree().get_nodes_in_group("collect_rock_label")[0]
 	for child in get_children():
@@ -29,19 +30,19 @@ func _physics_process(delta: float) -> void:
 	# 1 is left, -1 is right
 	var tilt : int = 0
 	if (Input.is_action_pressed("forward")):
-		direction += -camera.global_transform.basis.z.normalized()
+		direction -= camera.global_transform.basis.z.normalized()
 	if (Input.is_action_pressed("backwards")):
 		direction += camera.global_transform.basis.z.normalized()
 	if (Input.is_action_pressed("left")):
 		tilt = 1
-		direction += -camera.global_transform.basis.x.normalized()
+		direction -= camera.global_transform.basis.x.normalized()
 	if (Input.is_action_pressed("right")):
 		tilt = -1
 		direction += camera.global_transform.basis.x.normalized()
 	if(Input.is_action_pressed("up")):
-		direction.y += 1
+		direction += camera.global_transform.basis.y.normalized()
 	if(Input.is_action_pressed("down")):
-		direction.y -= 1
+		direction -= camera.global_transform.basis.y.normalized()
 	velocity = direction * speed
 	
 	# Camera tilt
@@ -65,13 +66,7 @@ func _physics_process(delta: float) -> void:
 		
 	
 func _input(event: InputEvent) -> void:
-	# Capture mouse if not captured and clicking the window
-	if (event is InputEventMouseButton):
-		if (event.pressed && event.button_index == 1):
-			if(Input.mouse_mode == Input.MOUSE_MODE_VISIBLE):
-				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
-	# Rotate the camera view
+		# Rotate the camera view
 	if (event is InputEventMouseMotion):
 		if(Input.mouse_mode != 0):
 			print(event.relative)
@@ -79,6 +74,13 @@ func _input(event: InputEvent) -> void:
 			if(new_rot < 90 && new_rot > -90):
 				camera.rotation_degrees.x = new_rot
 			camera.rotation_degrees.y += -event.screen_relative.x * mouse_speed
+		return
+	# Capture mouse if not captured and clicking the window
+	if (event is InputEventMouseButton):
+		if (event.pressed && event.button_index == 1):
+			if(Input.mouse_mode == Input.MOUSE_MODE_VISIBLE):
+				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		return
 	
 	# Escape mouse capture
 	if (event.is_action_pressed("escape")):
@@ -89,4 +91,3 @@ func _input(event: InputEvent) -> void:
 		if(collect_rock_label.visible):
 			if(current_rock != null):
 				current_rock.free()
-			
