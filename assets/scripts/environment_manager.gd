12815a6d8af12d97b7a1f0c8de46073fe5c4a22d
JSaurusRex <jsaurusrex22@gmail.com>
2026-07-25 19:07:28 +0200

    small changes

--- assets/scripts/environment_manager.gd
+++ assets/scripts/environment_manager.gd
@@ -5,9 +5,18 @@ extends WorldEnvironment
 
 var current_environment = -1;
 
+var player;
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+
 func set_env(num : int):
 	if num == current_environment:
 		return
 	print("setting environment to ", environments[num].to_string(), " from ", environments[current_environment].to_string())
 	environment = environments[num];
 	current_environment = num
+
+func _physics_process(delta: float) -> void:
+	if player.planet_exploding_timer < 0 && current_environment == 0:
+		environment.volumetric_fog_albedo = environment.volumetric_fog_albedo.lerp(Color(0.971, 0.641, 0.817), delta*0.15)
--- assets/scripts/mamga_growing.gd
+++ assets/scripts/mamga_growing.gd
@@ -1,10 +1,14 @@
 extends Node3D
 
 var size : Vector3
+var height : float
 
 func _ready():
 	size = scale
+	height = global_position.y
+	global_position.y = 0
 	scale *= 0.01
 
 func _process(delta: float) -> void:
 	scale = scale.move_toward(size, delta * size.y / 4.0)
+	global_position.y = move_toward(global_position.y, height, delta*3)
