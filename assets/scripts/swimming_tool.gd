6e7c94f8996c3476796d9d2f24f3626f085d676c
Dare <8192966+daredevil28@users.noreply.github.com>
2026-07-25 23:25:53 +0200

    Added audio for swimming tool

--- assets/scripts/swimming_tool.gd
+++ assets/scripts/swimming_tool.gd
@@ -6,21 +6,27 @@ var propellors : Node3D
 
 var speed : float = 0
 var velocity : Vector3 = Vector3.ZERO
+@onready var propellor_hum : AudioStreamPlayer = $AudioStreamPlayer
 
 func _ready():
 	propellors = get_node("propellors")
-
+	
 func do_action():
 	is_active = !is_active
 
 func _physics_process(delta: float) -> void:
+	if (propellor_hum.stream_paused):
+		propellor_hum.stream_paused = false
+		
 	if !visible:
 		is_active = false
 	
 	if is_active:
 		speed = move_toward(speed, 10, delta*5.5)
+		propellor_hum.volume_linear = 0.2
 	else:
 		speed = move_toward(speed, 0, delta*5.5)
+		propellor_hum.volume_linear = 0.0
 	
 	if visible:
 		velocity = velocity.lerp(-$"../../..".global_basis.z * speed, delta)
